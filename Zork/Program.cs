dd153d66a2e0966b4f04367df95113d3f4e5ffea
Lilmanlogy <89665049+Lilmanlogy@users.noreply.github.com>
2022-08-31 14:28:00 -0400

    Zork 1.2 Final Commit

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -10,26 +10,14 @@ namespace Zork
 
             string inputString = Console.ReadLine().Trim().ToUpper();
             Commands command = ToCommand(inputString); //Refactoring
-            
-            if (command == Commands.QUIT)
-            {
-                Console.WriteLine("Thank you for playing.");
-            }
-            else if (command == Commands.LOOK)
-            {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door");
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command");
-            }
+            Console.WriteLine(command);
         }
 
         private static Commands ToCommand(string commandString)
         {
-            if (Enum.TryParse<Commands>(commandString, true, out Commands command))
+            if (Enum.TryParse<Commands>(commandString, true, out Commands result))
             {
-                return command;
+                return result;
             }
             else
             {
