484c9800fa54555dad7016c2b9d609e6b1585df1
Lilmanlogy <89665049+Lilmanlogy@users.noreply.github.com>
2022-09-19 15:07:13 -0400

    Zork 3.2 Initial Commit
    Fixed error on line 47

--- Zork/Room.cs
+++ Zork/Room.cs
@@ -6,7 +6,7 @@
         
         public string Description { get; set; }
 
-        public Room(string name, string description = "")
+        public Room(string name, string description = null)
         {
             Name = name;
             Description = description;
